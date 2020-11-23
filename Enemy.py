798c4cfa16c29b89614aeab13931ba1484224705
MightGit <74358998+MightGit@users.noreply.github.com>
2020-11-23 14:02:30 +0100

    Revert "Revert "Merge remote-tracking branch 'upstream/master'""
    This reverts commit 6734f163db4f10ba2eca57a2169c34f80c3a97c8.

--- Enemy.py
+++ Enemy.py
@@ -1,28 +1,69 @@
 import pygame
 
 
+
+
 class EnemyClass:
+    xSpeed=0
+    ySpeed=0
+    maxSpeed=5
+    width=20
+    height=20
+    color=(255, 128, 255)
+    points=0
+    collisionSFX = pygame.mixer.Sound('aaw.wav')
 
-    width=16
-    height=16
-    color=(255 , 0, 128)
 
-
-    effect = pygame.mixer.Sound('plingpling.wav')
-
-    def __init__(self,screen, spawnPosX, spawnPosY, speedX, speedY):
-        self.x = spawnPosX
-        self.y = spawnPosY
-        self.xSpeed = speedX
-        self.ySpeed = speedY
+    def __init__(self,screen,xpos,ypos,terrainCollection):
+        self.x=xpos
+        self.y=ypos
         self.theScreen=screen
+        self.screenWidth = self.theScreen.get_size()[0] #
+        self.screenHeight = self.theScreen.get_size()[1]
+        self.terrainCollection=terrainCollection
 
     def update(self):
-        self.x+=self.xSpeed
-        self.y+=self.ySpeed
+
+        self.futureX=self.x+self.xSpeed
+        self.futureY=self.y+self.ySpeed
+
+        xWillCollide = False
+        yWillCollide = False
+
+        for tile in self.terrainCollection:
+            #if the player is within the x coordinates of a wall tile, and future Y coordinate is inside the wall:
+            if self.x + self.width > tile.x and self.x < tile.x + tile.width and self.futureY + self.height > tile.y and self.futureY < tile.y + tile.height:
+                yWillCollide=True
+            # if the player is within the Y coordinates of a wall tile, and future X coordinate is inside the wall:
+            if self.y + self.height > tile.y and self.y < tile.y + tile.height and self.futureX + self.width > tile.x and self.futureX < tile.x + tile.width:
+                xWillCollide=True
+
+        if not xWillCollide:
+            self.x = self.futureX
+        if not yWillCollide:
+            self.y = self.futureY
+
+        #safety to prevent overshoot:
+        if self.x+self.width > self.screenWidth:
+            self.x = self.screenWidth-self.width
+        if self.y+self.height > self.screenHeight:
+            self.y = self.screenHeight-self.height
+        if self.x<0:
+            self.x=0
+        if self.y<0:
+            self.y=0
+        from main import playerObject
+
+        if self.x < playerObject.x:
+            self.xSpeed =+ 1
+        if self.y < playerObject.y:
+            self.ySpeed =+ 1
+
+        if self.x > playerObject.x:
+            self.xSpeed =- 1
+        if self.y > playerObject.y:
+            self.ySpeed =- 1
+
 
     def draw(self):
         pygame.draw.rect(self.theScreen, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
-
-    def playSound(self):
-        self.effect.play()
