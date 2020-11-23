dd20871db7fc83c96a94e23dab58c6f40c202233
MightGit <74358998+MightGit@users.noreply.github.com>
2020-11-23 14:02:30 +0100

    Revert "Revert "Merge remote-tracking branch 'upstream/master'""
    This reverts commit 6734f163db4f10ba2eca57a2169c34f80c3a97c8.

---
+++ Alger.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+class AlgerClass:
+    color = (114, 222, 116)
+
+    def __init__(self, screen, _x, _y, _width, _height):
+        self.theScreen = screen
+        self.x = _x
+        self.y = _y
+        self.width = _width
+        self.height = _height
+
+    def draw(self):
+        pygame.draw.rect(self.theScreen, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
